b671eb17c36226c3da4bbb01fc697874fc1302a9
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-10-19 10:29:18 +0900

    f

--- airflow/models.py
+++ airflow/models.py
@@ -4141,21 +4141,19 @@ class XCom(Base, LoggingMixin):
 
         result = query.first()
 
-        if result is None:
-            return result
-
-        enable_pickling = configuration.getboolean('core', 'enable_xcom_pickling')
-        if enable_pickling:
-            return pickle.loads(result.value)
-        else:
-            try:
-                return json.loads(result.value.decode('UTF-8'))
-            except ValueError:
-                self.log.error("Could not deserialize the XCOM value from JSON. "
-                               "If you are using pickles instead of JSON "
-                               "for XCOM, then you need to enable pickle "
-                               "support for XCOM in your airflow config.")
-                raise
+        if result:
+            enable_pickling = configuration.getboolean('core', 'enable_xcom_pickling')
+            if enable_pickling:
+                return pickle.loads(result.value)
+            else:
+                try:
+                    return json.loads(result.value.decode('UTF-8'))
+                except ValueError:
+                    self.log.error("Could not deserialize the XCOM value from JSON. "
+                                   "If you are using pickles instead of JSON "
+                                   "for XCOM, then you need to enable pickle "
+                                   "support for XCOM in your airflow config.")
+                    raise
 
     @classmethod
     @provide_session
